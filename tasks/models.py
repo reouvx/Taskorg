8e3eddacfa2e9bd2d6802f13490d1377cacc0b5b
raseel <113070739+reouvx@users.noreply.github.com>
2024-03-27 09:17:08 +0300

    Update models.py

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateField()
+    is_completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+    
+    
